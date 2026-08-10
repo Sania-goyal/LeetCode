fd9c1c7522576a683eeae1d183ec28af3af538e2
Sania Goyal <25BCS10266@cuchd.in>
2026-08-10 11:30:24 +0530

    Time: 0 ms (100%), Space: 15.8 MB (13.9%) - LeetHub

---
+++ 0462-minimum-moves-to-equal-array-elements-ii/0462-minimum-moves-to-equal-array-elements-ii.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minMoves2(vector<int>& nums) {
+      sort(nums.begin(),nums.end());
+      int mid = nums.size()/2;
+      int moves =0;
+      for(int i=0;i<nums.size();i++)
+      {
+        moves = moves + abs((nums[mid]-nums[i]));
+      } 
+      return moves; 
+    }
+};
