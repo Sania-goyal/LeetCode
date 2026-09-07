2a711659ce108090f3a87db307ca33e9158bdb7a
Sania Goyal <25BCS10266@cuchd.in>
2026-09-07 21:20:41 +0530

    Time: 4 ms (59.17%), Space: 28.4 MB (83.75%) - LeetHub

---
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        int n= nums.size();
+        vector<int>v(n,-1);
+        stack<int>s;
+        for(int i=0;i<2*n;i++)
+        {
+            while(!s.empty() && nums[i%n]>nums[s.top()])
+            {
+                v[s.top()]=nums[i%n];
+                s.pop();
+            }
+            if(i<n){
+            s.push(i);
+            }
+        }
+        return v;
+    }
+};
